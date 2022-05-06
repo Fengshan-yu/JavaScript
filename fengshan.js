700b4470bcdfe573306c1e601dcbccdee81ef0e3
Fengshan Yu <95190262+Fengshan-yu@users.noreply.github.com>
2022-05-05 20:20:06 -0400

    Add files via upload
    Accidently signed up for a JavaScript course when I actually wanted Python Essential 2 through Python Institute. Just quickly went through the course and wrote up some very naive code.

---
+++ fengshan.js
@@ -0,0 +1,4 @@
+console.log("Hello, I am Fengshan Yu");
+console.log("I received my medical degree in China and earned my ECFMG certificate in the United States.");
+console.log("I did my graduate study in Neurobiology and were further trained in neuroscience research at Stanford University and NIH.");
+console.log("I am currently PCAP-Ceritified Associate in Python Programming.");
